fcb3e43d902894a04dc93436b03e02462fbc15b5
drmas001 <git@stackblitz.com>
2024-11-08 15:33:25 +0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  server: {
+    host: true,
+    strictPort: true,
+    port: 5173
+  },
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', '@supabase/supabase-js'],
+          ui: ['lucide-react', 'date-fns', 'recharts'],
+        }
+      }
+    }
+  }
+});
